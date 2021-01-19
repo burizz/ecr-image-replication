e94c7a7f19fc6d7db47c626edb40b405d8d4fa56
Boris Yakimov <yakimovb@gmail.com>
2021-01-19 09:00:11 +0200

    docker tag, push, and login methods

--- docker/docker.go
+++ docker/docker.go
@@ -23,7 +23,7 @@ func PullImage(imageTag string) (imagePullErr error) {
 
 	// ImagePull makes an API request to Docker daemon to pull the image, we don't actually pull it and store it ourselves here
 	reader, dockerPullErr := dockerClient.ImagePull(dockerCtx, imageTag, types.ImagePullOptions{})
-	defer reader.Close()
+	//defer reader.Close()
 	if dockerPullErr != nil {
 		return fmt.Errorf("cannot download image: %v", dockerPullErr)
 	}
@@ -35,8 +35,7 @@ func PullImage(imageTag string) (imagePullErr error) {
 	//if readOutputErr != nil {
 	//return fmt.Errorf("cannot read docker pull output: %v", readOutputErr)
 	//}
-
-	log.Infof("Test: %v", string(contents))
+	//log.Infof("Test: %v", string(contents))
 
 	return nil
 }
@@ -60,5 +59,62 @@ func ListImages() (listImagesErr error) {
 		log.Infof("Image tag: %v", image.RepoTags)
 	}
 
+	return nil
+}
+
+// TagImage - changes image tag, e.g. registry.example.com/myimage:latest
+func TagImage(sourceImageTag string, targetImageTag string) (ok bool, imageTagErr error) {
+	dockerCtx := context.Background()
+
+	dockerClient, initClientErr := config.DockerClientInit()
+	if initClientErr != nil {
+		return false, initClientErr
+	}
+
+	if imageTagErr := dockerClient.ImageTag(dockerCtx, sourceImageTag, targetImageTag); imageTagErr != nil {
+		return false, fmt.Errorf("cannot tag image; source tag [%v] - target tag [%v]; Error: %v", sourceImageTag, targetImageTag, imageTagErr)
+	}
+
+	log.Infof("Image tagged successfully; source tag [%v] - target tag [%v]", sourceImageTag, targetImageTag)
+	return true, nil
+}
+
+// PushImage - to private registry; image must already have tag which references the registry, e.g. registry.example.com/myimage:latest
+func PushImage(imageTag string) (imagePushErr error) {
+	dockerCtx := context.Background()
+
+	dockerClient, initClientErr := config.DockerClientInit()
+	if initClientErr != nil {
+		return initClientErr
+	}
+
+	reader, imagePushErr := dockerClient.ImagePush(dockerCtx, imageTag, types.ImagePushOptions{})
+	if imagePushErr != nil {
+		return fmt.Errorf("cannot push image: %v, %v", imageTag, imagePushErr)
+	}
+
+	// TODO: change this to logrus as well
+	// Sends ImagePush output via reader - to show download progress
+	io.Copy(os.Stdout, reader)
+
+	return nil
+}
+
+// DockerLogin - to registry
+func DockerLogin(registryName string) (dockerLoginErr error) {
+	dockerCtx := context.Background()
+
+	dockerClient, initClientErr := config.DockerClientInit()
+	if initClientErr != nil {
+		return initClientErr
+	}
+
+	auth, dockerLoginErr := dockerClient.RegistryLogin(dockerCtx, types.AuthConfig{})
+	if dockerLoginErr != nil {
+		return fmt.Errorf("cannot login to docker registry %v ; %v", registryName, dockerLoginErr)
+	}
+
+	//TODO: finish this
+	fmt.Println(auth)
 	return nil
 }
--- ecr-image-replication.go
+++ ecr-image-replication.go
@@ -20,13 +20,25 @@ func main() {
 	// TODO: Get Docker images from env variable passed into Docker
 	//dockerImages = os.Getenv("DOCKER_IMAGES")
 	var dockerImages = []string{"hello-world:latest"}
+	var ecrRegistry = ""
+
+	// Pull images
 	for _, imageTag := range dockerImages {
 		log.Infof("Pulling image [%v]", imageTag)
 		if imagePullErr := docker.PullImage(imageTag); imagePullErr != nil {
-			log.Fatalf("Error: %v", imagePullErr)
+			log.Errorf("Error: %v", imagePullErr)
+		}
+
+		ok, imageTagErr := docker.TagImage(imageTag, ecrRegistry)
+		if !ok || imageTagErr != nil {
+			log.Errorf("Error: %v", imageTagErr)
 		}
 	}
+
+	// List local images
 	if listImageErr := docker.ListImages(); listImageErr != nil {
 		log.Errorf("Error: %v", listImageErr)
 	}
+
+	// TODO: cleanup local images
 }
